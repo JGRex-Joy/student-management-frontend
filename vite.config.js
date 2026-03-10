5a17351b3c5c3c6b7b8d3ad18b90aece2a972b6d
JGRex-Joy <omurkulovamir2008@gmail.com>
2026-03-10 12:55:18 +0600

    v2.0

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react/',
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8080',
+      '/login': 'http://localhost:8080',
+      '/logout': 'http://localhost:8080',
+      '/dashboard': 'http://localhost:8080',
+    }
+  },
+  build: {
+    outDir: '../src/main/resources/static/react',
+    emptyOutDir: true,
+  }
 })
